bf17ed956c6b7955abd1ae01c70a2fb8e2b37100
DarshanParmar <>
2026-01-26 14:06:56 +0530

    Project Add

---
+++ CozyCouplesTests/CozyCouplesTests.swift
@@ -0,0 +1,17 @@
+//
+//  CozyCouplesTests.swift
+//  CozyCouplesTests
+//
+//  Created by Darshan on 26/01/26.
+//
+
+import Testing
+@testable import CozyCouples
+
+struct CozyCouplesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
